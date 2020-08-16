c3fff2277ff919e3094adbb24c66b6ce4a4c3cad
Canato <vcanato@gmail.com>
2020-08-16 11:32:47 +0100

    Transform all gradles to kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(Apps.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(Apps.minSdk)
+        targetSdkVersion (Apps.targetSdk)
+        versionCode = Apps.versionCode
+        versionName = Apps.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles (getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation (fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation (Libs.kotlin.stdLib)
+
+    implementation (Libs.android.coreKtx)
+    implementation (Libs.android.appCompat)
+    implementation (Libs.android.constraintLayout)
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = "1.3.72"
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:4.0.0"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-
+        classpath (Libs.android.gradle)
+        classpath (Libs.kotlin.gradle)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -18,9 +16,10 @@ allprojects {
     repositories {
         google()
         jcenter()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean",Delete::class){
+    delete(rootProject.buildDir)
 }
