6e86d193e8635b2128f8ccc39d6d65f4f4795621
Canato <vcanato@gmail.com>
2020-08-16 13:07:16 +0100

    [KtLint-Bot]: Fixing your shit code

--- app/src/main/java/com/can_apps/showcase/MainActivity.kt
+++ app/src/main/java/com/can_apps/showcase/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    //TEST
+    // TEST
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
