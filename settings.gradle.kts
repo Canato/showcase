841b3ebe7641d051df3ac16b061b5fc5480785d3
Canato <vcanato@gmail.com>
2020-10-28 14:09:55 +0000

    run refresh command

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 import de.fayard.refreshVersions.bootstrapRefreshVersions
+import de.fayard.refreshVersions.migrateRefreshVersionsIfNeeded
 
 rootProject.buildFileName = "build.gradle.kts"
 rootProject.name = "Showcase"
@@ -22,6 +23,10 @@ include(
 buildscript {
     repositories { gradlePluginPortal() }
     dependencies.classpath("de.fayard.refreshVersions:refreshVersions:0.9.5")
+////                                                      # available:0.9.6")
+////                                                      # available:0.9.7")
 }
+
+migrateRefreshVersionsIfNeeded("0.9.5") // Will be automatically removed by refreshVersions when upgraded to the latest version.
 
 bootstrapRefreshVersions()
