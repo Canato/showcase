96de13c309b3068238cea742f3ab37cca4f29102
Canato <Canato@users.noreply.github.com>
2021-08-09 06:23:00 +0000

    CI - Add new versions if exist

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,6 +27,7 @@ buildscript {
     dependencies.classpath("de.fayard.refreshVersions:refreshVersions:0.9.7")
 ////                                                      # available:0.10.0")
 ////                                                      # available:0.10.1")
+////                                                      # available:0.11.0")
 }
 
 migrateRefreshVersionsIfNeeded("0.9.7") // Will be automatically removed by refreshVersions when upgraded to the latest version.
